1ad3a686d8b19fa8b377c42383162c6019a52b5d
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2022-09-26 14:32:23 -0300

    Adicionar arquivos de projeto.

---
+++ SobrecargaOperador/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace SobrecargaOperador
+{
+    class Ponto
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+
+        public Ponto()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Ponto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public string imprime()
+        {
+            return $"X = {this.x} e Y = {this.y}";
+        }
+
+        public static Ponto operator ++(Ponto p)
+        {
+            Ponto pt = p;
+
+            pt.x = pt.x + 1;
+            pt.y = pt.y + 1;
+
+            return pt;
+        }
+        public static Ponto operator +(Ponto p1, Ponto p2)
+        {
+            Ponto pt = new Ponto();
+
+            pt.x = p1.x + p2.x;
+            pt.y = p1.y + p2.y;
+
+            return pt;
+            
+        }
+        class Program
+        {
+            static void Main()
+            {
+                Ponto p = new Ponto();
+                Ponto pp = new Ponto(2, 6);
+                Ponto ppp = new Ponto(4, 4);
+
+                p = pp + ppp;
+
+                Console.WriteLine(p.x);
+                Console.WriteLine(p.y);
+
+                p = p++;
+
+                Console.WriteLine(p.x);
+                Console.WriteLine(p.y);
+
+            }
+        }
+
+    }
+}
